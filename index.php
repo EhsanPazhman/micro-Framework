54509e4f1d97432420df5bdcc1936e1a7270cecb
Ehsan-Pazhman <ehsanpazhman@gmail.com>
2024-06-28 21:57:20 +0430

    bugs fixed

--- index.php
+++ index.php
@@ -3,21 +3,5 @@ use App\Core\Routing\Router;
 use App\Models\Product;
 use App\Models\User;
 include "bootstrap/init.php";
-// $user_data = [
-//     'name' => 'Negin' ,
-//     'email' => 'Negin@gmail.com' ,
-//     'password' => 'Negin@test' 
-// ];
-// $userModel = new User();
-// $user = $userModel->sum('id',['id[>]'=>55]);
-// for ($i=0; $i < 20; $i++) { 
-//     $userModel->create([
-//     'name' => "user-$i" ,
-//     'email' => "email-$i@gmail.com" ,
-//     'password' => "pass-$i" 
-//     ]);
-// }
-// var_dump( $user);
-
 $router = new Router();
 $router->run();
--- views/home/index.php
+++ views/home/index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Archive here ...! </title>
+    <title>Welcome</title>
 </head>
-<body style="background-color: #a79f9f;">
-<h3 style="margin: 100px; text-align: center; color: #060606;">Archive here ...!</h3>
+<body style="background-color: #121a12;">
+<h1 style="margin: 18%; text-align: center; margin-bottom: -100px; color: #ff8686;">Welcome ....</h1>
 </body>
 </html>
